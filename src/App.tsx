14c08b128c94fe09d2a5c1536e82ed991219a75e
Marvin Schopf <marvin@schopf.biz>
2021-02-26 19:53:29 +0100

    Add basic home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import Nimiq from "@nimiq/core-web"
 
 export default class App extends Component {
 	render() {
-		return <Fragment></Fragment>
+		return (
+			<Fragment>
+				<header id="top" className="nq-blue-bg">
+					<h1 className="nq-h1">NIMIQ.link</h1>
+					<p className="nq-label">
+						URL shortener with NIMIQ integration
+					</p>
+				</header>
+				<div className="content">
+					<div className="nq-card">
+						<div className="nq-card-header">
+							<h1 className="nq-h1">Create shortlink</h1>
+						</div>
+						<div className="nq-card-body"></div>
+					</div>
+				</div>
+			</Fragment>
+		)
 	}
 }
