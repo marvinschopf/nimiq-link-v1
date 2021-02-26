4cb227a74ce9c796d79bedfe85e9e0a93de14e3e
Marvin Schopf <marvin@schopf.biz>
2021-02-26 18:51:11 +0100

    setup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import alias from "@rollup/plugin-alias"
 import typescript from "@rollup/plugin-typescript"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
+import html from "@rollup/plugin-html"
 
 module.exports = {
 	input: "src/index.tsx",
+	treeshake: true,
+	preserveEntrySignatures: false,
 	output: {
-		file: "dist/bundle.js",
+		sourcemap: false,
 		format: "iife",
+		name: "app",
+		dir: "dist",
+		entryFileNames: "bundle.[hash].js",
 	},
 	plugins: [
 		nodeResolve(),
@@ -16,6 +22,19 @@ module.exports = {
 				{ find: "react", replacement: "preact/compat" },
 				{ find: "react-dom", replacement: "preact/compat" },
 			],
+		}),
+		html({
+			fileName: "index.html",
+			meta: [
+				{
+					charset: "utf-8",
+				},
+				{
+					name: "viewport",
+					content: "width=device-width,initial-scale=1.0",
+				},
+			],
+			title: "NIMIQ.link",
 		}),
 	],
 }
