0f93626296994f82550dca300d3d1534d8fad0db
Marvin Schopf <marvin@schopf.biz>
2021-02-27 00:02:30 +0100

    fix api route

--- api/create.ts
+++ api/create.ts
@@ -2,8 +2,9 @@ import { NowRequest, NowResponse } from "@vercel/node"
 
 export default (_req: NowRequest, res: NowResponse) => {
 	if (
-		(_req.body.target && _req.body.wallet) ||
-		(_req.query.target && _req.query.wallet)
+		(_req.body || _req.query) &&
+		((_req.body.target && _req.body.wallet) ||
+			(_req.query.target && _req.query.wallet))
 	) {
 		const target: string = _req.body.target || _req.query.target
 		const wallet: string = _req.body.wallet || _req.query.wallet
