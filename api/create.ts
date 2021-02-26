b3b76c0c9747954bb30ca6e732caa594e6dd815c
Marvin Schopf <marvin@schopf.biz>
2021-02-26 23:50:03 +0100

    add basic api handler

---
+++ api/create.ts
@@ -0,0 +1,18 @@
+import { NowRequest, NowResponse } from "@vercel/node"
+
+export default (_req: NowRequest, res: NowResponse) => {
+	if (
+		(_req.body.target && _req.body.wallet) ||
+		(_req.query.target && _req.query.wallet)
+	) {
+		const target: string = _req.body.target || _req.query.target
+		const wallet: string = _req.body.wallet || _req.query.wallet
+		const shares: number = _req.body.shares || _req.query.shares || 1
+		res.status(200).json({ target, wallet, shares })
+	} else {
+		res.status(422).json({
+			success: false,
+			error: "PARAMETER_MISSING",
+		})
+	}
+}
