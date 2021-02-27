cf8a43afcc6c8ff32f3d993d417d725301cce0cc
Marvin Schopf <marvin@schopf.biz>
2021-02-27 11:05:50 +0100

    fix api create route

--- api/create.ts
+++ api/create.ts
@@ -1,14 +1,17 @@
 import { NowRequest, NowResponse } from "@vercel/node"
 
 export default (_req: NowRequest, res: NowResponse) => {
-	if (
-		(_req.body || _req.query) &&
-		((_req.body.target && _req.body.wallet) ||
-			(_req.query.target && _req.query.wallet))
-	) {
-		const target: string = _req.body.target || _req.query.target
-		const wallet: string = _req.body.wallet || _req.query.wallet
-		const shares: number = _req.body.shares || _req.query.shares || 1
+	let target: string = ""
+	let wallet: string = ""
+	let shares: number = 1
+	if (_req.query && _req.query.target) target = _req.query.target.toString()
+	if (_req.query && _req.query.wallet) wallet = _req.query.wallet.toString()
+	if (_req.query && _req.query.shares)
+		shares = parseInt(_req.query.shares.toString())
+	if (_req.body && _req.body.target) target = _req.body.target
+	if (_req.body && _req.body.wallet) wallet = _req.body.wallet
+	if (_req.body && _req.body.shares) shares = _req.body.shares
+	if (target.length >= 1 && wallet.length >= 1 && shares > 0) {
 		res.status(200).json({ target, wallet, shares })
 	} else {
 		res.status(422).json({
